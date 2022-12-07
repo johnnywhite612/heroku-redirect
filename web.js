6201f9314be57cf03757e8a04816152182a986c0
johnnywhite612 <johnnywhite612@gmail.com>
2022-12-07 21:15:36 +0000

    Update

--- web.js
+++ web.js
@@ -1,14 +1,15 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-var newBaseURL = process.env.NEW_BASE_URL || 'http://example.com';
+var newBaseURL =
+  process.env.NEW_BASE_URL || "https://johnnywhite612.github.io/CertBank/";
 var redirectStatus = parseInt(process.env.REDIRECT_STATUS || 302);
 var port = process.env.PORT || 5000;
 
-app.get('*', function(request, response) {
+app.get("*", function (request, response) {
   response.redirect(redirectStatus, newBaseURL + request.url);
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("Listening on " + port);
 });
